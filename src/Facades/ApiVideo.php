876d809ae055df3ecb77437e546e687fe20268c4
Duncan McClean <duncan@steadfastcollective.com>
2021-06-22 11:05:40 +0100

    wip

--- src/Facades/ApiVideo.php
+++ src/Facades/ApiVideo.php
@@ -4,7 +4,12 @@ namespace SteadfastCollective\ApiVideo\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use SteadfastCollective\ApiVideo\ApiVideo as BaseApiVideo;
-
+/**
+ * @method static string getDelegateToken()
+ * @method static \ApiVideo\Client\Model\Video getVideo(string $videoId)
+ * @method static \ApiVideo\Client\Model\Video updateVideo(string $videoId, array $params)
+ * @method static int deleteVideo(string $videoId)
+ */
 class ApiVideo extends Facade
 {
     public static function getFacadeAccessor()
